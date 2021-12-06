295c4d74f9b67383befcc5b978d13c4f80c8fe33
Ian Molina <ianmg.daw@gmail.com>
2021-12-06 10:28:57 +0100

    Juego sudoku v4

--- JuegoSudoku/src/Sudoku.java
+++ JuegoSudoku/src/Sudoku.java
@@ -12,19 +12,19 @@ import java.util.Scanner;
  * @version 1.0
  * @date 1 dic. 2021 21:29:58
  */
-
 public class Sudoku {
 //VARIABLES GLOBALES - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     public static Scanner in = new Scanner(System.in);
     public static int opcion; //opción del menú
-    public static int[][] sudoku = new int[8][8];
+    public static int[][] sudoku = new int[4][4];
+    public static boolean salir = false;
+    public static int posC, posF, num;
 
 //LISTA DE FUNCIONES - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    
     //Crear sudoku 4x4 =========================================================
     public static int[][] crearSudoku4x4() {
-        
+
         for (int i = 0; i < sudoku.length; i++) { //fila
             for (int j = 0; j < sudoku[i].length; j++) { //columna
                 sudoku[i][j] = 0;
@@ -34,15 +34,16 @@ public class Sudoku {
         return sudoku;
     }
     //==========================================================================
-    
+
     //Mostrar sudoku 4x4 =======================================================
     public static int[][] mostrarSudoku4x4() {
+        System.out.println("C:   0 1 2 3 4");
         for (int i = 0; i < sudoku.length; i++) { //fila
-            System.out.print("F" + i +": ");
+            System.out.print("F" + i + ": ");
             for (int j = 0; j < sudoku[i].length; j++) { //columna
                 System.out.print(sudoku[i][j] + " ");
             }
-            System.out.println(""); 
+            System.out.println("");
         }
         return sudoku;
     }
@@ -64,7 +65,6 @@ public class Sudoku {
     //Pide al jugador un valor INT, umenuna y otra vez hasta que ===============
     //responde con valor en rango
     public static int pedirIntEnRango(int min, int max) {
-        
         int valor;
         do {
             valor = in.nextInt();
@@ -78,11 +78,34 @@ public class Sudoku {
     }
     //==========================================================================
 
+    //==========================================================================
+
+    // Pedir el numero de posición de fila y de columna y un numero de 1 al 4 ==
+    // Despues se guarda el número
+    public static int[][] modificarSudoku() {
+        System.out.print("Escribe Nº posicion de fila: ");  //pedir fila
+        posF = pedirIntEnRango(0, 3);
+        System.out.print("Escribe Nº posicion de columna: "); //pedir columna
+        posC = pedirIntEnRango(0, 3);
+        System.out.print("Escribe el número: "); //pedir numero
+        num = pedirIntEnRango(1, 4);
+        
+        System.out.println("PosF: " + posF);
+        System.out.println("PosC: " + posC);
+        System.out.println("Num: " + num);
+
+        sudoku[posF][posC] = num;
+        
+        System.out.println("PRUEBA: " + sudoku[posF][posC]);
+        
+        return sudoku;
+    }
+    //==========================================================================
+
 //MAIN PRINCIPAL - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     public static void main(String[] args) {
 
         //VARIABLES AUXILIARES
-        
         //Bucle principal
         do {
             crearSudoku4x4(); //Crear la tabla de sudoku al empezar.
@@ -90,21 +113,23 @@ public class Sudoku {
 
             switch (opcion) {
                 case 0: //Salir
-
+                    salir = true;
                     break;
                 case 1: //Mostrar sudoku
                     mostrarSudoku4x4();
                     break;
                 case 2: //Añadir o modificar
-
+                    modificarSudoku();
                     break;
+
                 case 3: //Terminar
 
+                    salir = true;
                     break;
             }
 
             System.out.println("");
-        } while (opcion != 0);
+        } while (!salir);
 
     }
 }
